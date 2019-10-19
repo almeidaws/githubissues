f31dd9f2c8a145b914f45abb1fb9a3856d65b47b
Gustavo Almeida <almeidaws@outlook.com>
2019-10-19 15:24:20 -0300

    Create the Xcode project

---
+++ GitHubIssues/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  GitHubIssues
+//
+//  Created by Gustavo Amaral on 19/10/19.
+//  Copyright © 2019 Gustavo Almeida Amaral. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
